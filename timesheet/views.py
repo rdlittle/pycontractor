95500473b2aadbf02bc8712f11690f118a26ff18
rlittle <rlittle@littleacres.org>
2019-03-20 14:31:57 -0400

    Added delete timesheet feature.  Recalc total hours on timesheet edit/delete

--- timesheet/views.py
+++ timesheet/views.py
@@ -13,11 +13,37 @@ def timesheet_list():
     list = ts.find({}).sort('date', DESCENDING)
     return render_template('timesheet/list.html', items=list)
 
-@app.route('/delete/<int:tsid>', methods=['GET'])
-def timesheet_delete(tsid):
-    if tsid:
-        db.timesheet.delete_one({'_id': ObjectId(tsid)})
-    return redirect(url_for('timesheet_list'))
+@app.route('/delete/<int:invoice_id>/<int:tsid>', methods=['GET'])
+def timesheet_delete(invoice_id, tsid):
+    invoice = db.invoice.find_one({'_id': invoice_id})
+    if invoice:
+        # Like: db.invoice.find({_id: 378},{'detail': {$elemMatch: {'_id': 2776}}})
+        # ts = db.invoice.find_one({'_id': invoice_id}, {'detail': {'$elemMatch': {'_id': tsid}}})
+        # hours = float(ts['detail'][0]['hours'])
+        # inv = db.invoice.find_one({'_id': invoice_id})
+        # inv['hours'] -= hours
+        # inv['amount'] = float(inv['hours']) * 50.00
+
+        """ this removes the correct element in the mongo shell
+        db.invoice.updateOne({_id: 378}, {$pull: {detail: {'_id': 2782}}})
+        """
+        #pdb.set_trace()
+        #db.invoice.update_one({'_id': invoice_id}, {
+        #    '$pull': {'detail': {'_id': tsid}},
+        #    '$set': {'hours': inv['hours'], 'amount': inv['amount']}
+        #    })
+
+        qry = {'_id': invoice_id}
+        upd = {'$pull': {'detail': {'_id': tsid}}}
+        db.invoice.update_one(qry,upd)
+
+        invoice = db.invoice.find_one({'_id': invoice_id})
+        hours = 0
+        for ts in invoice['detail']:
+            hours += float(ts['hours'])
+        db.invoice.update_one({'_id': invoice_id}, {'$set': {'hours': hours}})
+
+    return redirect( url_for('invoice_edit',invoice_id=invoice_id))
 
 @app.route('/timesheet_entry/<int:invoice_id>')
 def timesheet_entry(invoice_id):
@@ -28,6 +54,9 @@ def timesheet_edit(inv_id, tsid):
     
     if 'cancel_button' in request.form:
         return redirect( url_for('invoice_edit',invoice_id=inv_id))
+
+    if 'delete_button' in request.form:
+        timesheet_delete(invoice_id=inv_id, tsid=tsid)
 
     invoice = db.invoice.find_one({'_id': inv_id})
     ts = None
@@ -46,6 +75,9 @@ def timesheet_edit(inv_id, tsid):
     ts['hours'] = request.form['hours']
 
     invoice['detail'][ts_index] = ts
+    invoice['hours'] = 0
+    for ts in invoice['detail']:
+        invoice['hours'] += float(ts['hours'])
 
     db.invoice.replace_one({'_id': inv_id}, invoice, True)
 
