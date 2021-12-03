e691790f24a9ade766d0faf43c9b4f3d02a52f3b
rlittle <rlittle@littleacres.org>
2021-12-03 12:04:34 -0500

    created dummy CRUD functions

--- util/db.py
+++ util/db.py
@@ -1,8 +1,22 @@
 class DB():
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         pass
     
-    def find(self):
+    def query(self, *args, **kwargs):
         pass
     
+    def create(self, *args, **kwargs):
+        pass
+    
+    def read(self, *args, **kwargs):
+        pass
+    
+    def update(self, *args, **kwargs):
+        pass
+    
+    def delete(self, *args, **kwargs):
+        pass
+    
+    def sort(self, *args, **kwargs):
+        pass
     
